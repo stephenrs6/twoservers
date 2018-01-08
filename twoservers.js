bf814377c7dcb558ef0fd083af3822544cd8ae51
stephen <stephenrs7@gmail.com>
2018-01-08 10:49:41 -0800

    aoaooadk

--- twoservers.js
+++ twoservers.js
@@ -1,23 +1,33 @@
-// Require/import the HTTP module
 var http = require("http");
 
-// Define a port to listen for incoming requests
-var PORT = 8080;
+var PORT = 7000;
+var PORT2 = 7500;
 
 // Create a generic function to handle requests and responses
 function handleRequest(request, response) {
 
   // Send the below string to the client when the user visits the PORT URL
-  response.end("It Works!! Path Hit: " + request.url);
+  response.end("Good Job!: " + request.url);
 }
 
+function handleRequest2(request, response) {
+
+    // Send the below string to the client when the user visits the PORT URL
+    response.end("Bad Job!: " + request.url);
+  }
 // Use the Node HTTP package to create our server.
 // Pass the handleRequest function to empower it with functionality.
 var server = http.createServer(handleRequest);
-
+var server2 = http.createServer(handleRequest2);
 // Start our server so that it can begin listening to client requests.
 server.listen(PORT, function() {
 
   // Log (server-side) when our server has started
   console.log("Server listening on: http://localhost:" + PORT);
 });
+
+server2.listen(PORT2, function() {
+
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT2);
+  });
